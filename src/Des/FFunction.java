dcd35792d5e144a5e48d55ea40de6b70eac2f3c2
arch <aa>
2019-04-09 23:55:53 +0200

    Add sBoxes to FFunction

--- src/Des/FFunction.java
+++ src/Des/FFunction.java
@@ -1,16 +1,19 @@
 package Des;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FFunction {
 
     // length 48
-    private byte[] extensionPermutationTable ={
+    private static byte[] extensionPermutationTable ={
             32,  1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,
              8,  9, 10, 11, 12, 12, 12, 13, 14, 15, 16, 17,
             16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
             24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,  1};
 
 
-    public byte[] fun(byte[] bytes, byte[] key){
+    public static byte[] fun(byte[] bytes, byte[] key){
         byte[][] splitArr = Utility.splitArrayInHalf(bytes);
 
         byte[] rSide = splitArr[1];
@@ -20,7 +23,23 @@ public class FFunction {
 
         byte[] xorArray = Utility.xorArrays(extendedArray, key);
 
+        byte[][] sBoxes = Utility.splitArrayInEigth(xorArray);
 
-        return new byte[1];
+        List<Byte> concatenatedSBox = new ArrayList<>();
+        byte[] product = new byte[32];
+
+        for (int i = 0; i < 8; i++) {
+            byte[] k = SBlock.get(i, sBoxes[i]);
+            for(byte s: k){
+                concatenatedSBox.add(s);
+            }
+        }
+
+        for (int i = 0; i < 32; i++) {
+            product[i] = concatenatedSBox.get(i);
+        }
+
+
+        return product;
     }
 }
