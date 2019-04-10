7fdeac864379559a23a28004a7a6dedaf5c71155
arch <aa>
2019-04-11 00:37:29 +0200

    some dummy tests

--- src/Des/Des.java
+++ src/Des/Des.java
@@ -20,17 +20,17 @@ public class Des {
             34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41,  9, 49, 17, 57, 25};
 
     // length 56
-    private byte[] keyPermutationTable = {
+    private static byte[] keyPermutationTable = {
             57, 49, 41, 33, 25, 17,  9,  1, 58, 50, 42, 34, 26, 18,
             10,  2, 59, 51, 43, 35, 27, 19, 11,  3, 60, 52, 44, 36,
             63, 55, 47, 39, 31, 23, 15,  7, 62, 54, 46, 38, 30, 22,
             14,  6, 61, 53, 45, 37, 29, 21, 13,  5, 28, 20, 12,  4};
 
     // length 16
-    private byte[] keyShiftTable = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+    private static byte[] keyShiftTable = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
     // length 12
-    private byte[] compressionPermutationTable = {
+    private static byte[] compressionPermutationTable = {
             14, 17, 11, 24,  1,  5,  3, 28, 15,  6, 21, 10,
             23, 19, 12,  4, 26,  8, 16,  7, 27, 20, 13,  2,
             41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
@@ -68,7 +68,7 @@ public class Des {
     }
 
     // round methods
-    private byte[] keyTransformation(byte[] key, int round){
+    public static byte[] keyTransformation(byte[] key, int round){
 
         int shift = keyShiftTable[round];
 
@@ -84,6 +84,15 @@ public class Des {
     public static byte[] initialPermutation(byte[] bits){
         return Utility.swapArrayElements(bits, initialPermutationTable);
     }
+
+    public static byte[] KeyPermutation(byte[] key){
+        return Utility.swapArrayElements(key, keyPermutationTable);
+    }
+
+    public static byte[] KeyCompression(byte[] key){
+        return Utility.swapArrayElements(key, compressionPermutationTable);
+    }
+
     private byte[] finalPermutation(byte[] bits){
         return Utility.swapArrayElements(bits, finalPermutationTable);
     }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,14 @@
 package sample;
 
+import Des.Utility;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import test.UtilityTest;
+
+import java.util.Arrays;
 
 //extends Application
 public class Main {
@@ -106,6 +109,45 @@ public class Main {
 
 
 //        launch(args);
+
+//         Tests xDDDD
+        byte[] cipher = strToArr("0010100111011000001000110011011100111100010101100100100111111011");
+        byte[] cipher2 = Des.Des.initialPermutation(cipher);
+        System.out.println("1110001010111010001110001100110110000010100111011101001110101100");
+        System.out.println(arrToStr(cipher2));
+////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("\nKey test");
+        byte[] key1 = strToArr("1101000111010100101000110101110010101010000100011101010010101010");
+        key1 = Des.Des.KeyPermutation(key1);
+        System.out.println(arrToStr(key1));
+
+        System.out.println("11010111010010111001010001101001010001001010100110001011");
+////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("\nSplit arr test");
+        byte[][] splited = Utility.splitArrayInHalf(key1);
+
+        System.out.println(Arrays.equals(strToArr("1101011101001011100101000110"), splited[0]));
+        System.out.println(Arrays.equals(strToArr("1001010001001010100110001011"), splited[1]));
+
+////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("\nShift test");
+
+        byte[] shiftedL =  Utility.shiftArray(splited[0],1);
+        byte[] shiftedR =  Utility.shiftArray(splited[1],1);
+
+        System.out.println(Arrays.equals(strToArr("1010111010010111001010001101"), shiftedL));
+        System.out.println(Arrays.equals(strToArr("0010100010010101001100010111"), shiftedR));
+////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("\nArray cat test");
+        byte[] catArr = Utility.catArrays(shiftedL, shiftedR);
+        System.out.println(Arrays.equals(strToArr("10101110100101110010100011010010100010010101001100010111"),
+                catArr));
+////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("\nCompression test");
+        byte[] compressed = Des.Des.KeyCompression(catArr);
+        System.out.println(Arrays.equals(strToArr("100011111110011010110000011111010011100100010000"),
+                compressed));
+
     }
 
     public static void printArr(byte[]arr){
@@ -123,5 +165,34 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+    }
+
+     static byte[] strToArr(String text){
+       byte[] result = new byte[text.length()];
+       int i = 0;
+       for (char ch: text.toCharArray()){
+           if(ch == '0'){
+               result[i] = 0;
+           }
+           else {
+               result[i] = 1;
+           }
+           i++;
+       }
+       return result;
+    }
+
+    static String arrToStr(byte[] bytes){
+        StringBuilder result = new StringBuilder();
+
+        for (byte b : bytes){
+            if(b == 1){
+                result.append("1");
+            }
+            else{
+                result.append("0");
+            }
+        }
+        return result.toString();
     }
 }
